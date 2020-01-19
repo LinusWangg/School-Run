1e287a2016e2a4f8fcf6b9f9941d6856fbab6936
LinusWangg <441656989@qq.com>
2020-01-19 14:57:02 +0800

    加入扫码功能

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,37 @@
 //logs.js
-const util = require('../../utils/util.js')
-
+var app = getApp()
 Page({
   data: {
-    logs: []
+    show: "",
   },
+
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+    console.log('onLoad')
+  },
+  click: function () {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        this.show = "结果:" + res.result + "\n二维码类型:" + res.scanType + "\n字符集:" + res.charSet + "\n路径:" + res.path;
+        that.setData({
+          show: this.show
+        })
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '失败',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      complete: (res) => {
+      }
     })
   }
 })
