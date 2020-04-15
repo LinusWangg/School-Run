a116f4b2b13acfca1af9c448e53935a944a57511
qinmaye <qinchi0518@gmail.com>
2020-04-15 08:52:25 +0800

    static dir

--- schoolrun4/schoolrun4/settings.py
+++ schoolrun4/schoolrun4/settings.py
@@ -128,10 +128,9 @@ USE_TZ = False
 
 STATIC_ROOT = '/var/School-Run/schoolrun4/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( '/var/School-Run/schoolrun4/static/' )
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/School-Run/schoolrun4/static/'
+]
 
 WX_APP_SECRET = '66d0351912116ab0371252bd0a088777'
