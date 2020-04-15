75bf5baa975306f4b2f7fb73f9694edc9125fac4
qinmaye <qinchi0518@gmail.com>
2020-04-15 08:53:42 +0800

    修改static

--- schoolrun4/schoolrun4/settings.py
+++ schoolrun4/schoolrun4/settings.py
@@ -126,7 +126,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-STATIC_ROOT = '/var/School-Run/schoolrun4/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"/static/")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
