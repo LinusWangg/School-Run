70cb1c56b1a0cf48479c46126632ad318f3e4cfc
LinusWangg <441656989@qq.com>
2020-10-12 09:02:29 +0800

    id传参问题

--- pages/location/location.js
+++ pages/location/location.js
@@ -135,6 +135,7 @@ Page({
     // 页面初始化 options为页面跳转所带来的参数
     this.getLocation();
     trace = JSON.parse(options.trace);
+    id = options.id;
     var length = trace.length;
     console.log(trace);
     console.log(length);
